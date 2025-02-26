c418ee04d7dec6e588074cf1cd96529e6629dbe2
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-26 18:18:02 +0200

    Created second dictionary

--- python-dictionaries.py
+++ python-dictionaries.py
@@ -1,11 +1,33 @@
 
+# created first dictionary
 dict1= {}
-letter = ""
+letter = input ("enter letter: ")
+
+
 while letter != "stop":
-    letter = input ("enter letter: ")
+    # letter = input ("enter letter: ")
     number = int(input("enter corresponding no.: "))
     print (letter,number)
     # dict1= {letter:number}
     # print(dict1)
     dict1[f"{letter}"]=number
-    print(dict1)
+    # print(dict1)
+    letter = input ("enter letter: ")
+print(dict1)
+
+# created second dictionary
+dict2={}
+letter = input ("enter letter: ")
+
+while letter != "stop":
+    # letter = input ("enter letter: ")
+    number = int(input("enter corresponding no.: "))
+    print (letter,number)
+    # dict1= {letter:number}
+    # print(dict1)
+    dict2[f"{letter}"]=number
+    # print(dict2)
+    letter = input ("enter letter: ")
+print(dict2)
+    
+# 
