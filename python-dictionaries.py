701c2748d232bb01ff62e36d9e4f69a46cc56ef1
Ahmad Saaty <ahmad.o.saaty@gmail.com>
2025-02-26 18:28:59 +0200

    Added merged dictionary

--- python-dictionaries.py
+++ python-dictionaries.py
@@ -2,18 +2,22 @@
 # created first dictionary
 dict1= {}
 letter = input ("enter letter: ")
-
+merge ={}
 
 while letter != "stop":
     # letter = input ("enter letter: ")
     number = int(input("enter corresponding no.: "))
-    print (letter,number)
+    # print (letter,number)
     # dict1= {letter:number}
     # print(dict1)
     dict1[f"{letter}"]=number
     # print(dict1)
+    merge [f"{letter}"]= number
+
     letter = input ("enter letter: ")
 print(dict1)
+print(merge)
+
 
 # created second dictionary
 dict2={}
@@ -22,12 +26,16 @@ letter = input ("enter letter: ")
 while letter != "stop":
     # letter = input ("enter letter: ")
     number = int(input("enter corresponding no.: "))
-    print (letter,number)
+    # print (letter,number)
     # dict1= {letter:number}
     # print(dict1)
     dict2[f"{letter}"]=number
     # print(dict2)
+    merge [f"{letter}"]= number
+
     letter = input ("enter letter: ")
 print(dict2)
+
+print(merge)
+
     
-# 
